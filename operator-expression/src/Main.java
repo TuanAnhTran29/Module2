3c014d9d5532e26bb8cfbdc5a256d177f5f27747
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2021-11-15 20:10:10 +0700

    Added folder operator-expression/

---
+++ operator-expression/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width= scanner.nextFloat();
+
+
+        System.out.printf("Enter height: ");
+        height= scanner.nextFloat();
+
+        float area= width * height;
+
+        System.out.printf("Area is: " + area);
+
+    }
+}
