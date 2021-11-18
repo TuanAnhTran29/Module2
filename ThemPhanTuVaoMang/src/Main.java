798cf7ccc02ab3404790c6eb3fa428d05cbb87be
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2021-11-18 14:31:22 +0700

    Added folder ThemPhanTuVaoMang/

--- ThemPhanTuVaoMang/src/Main.java
+++ ThemPhanTuVaoMang/src/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        int[] arr= {1,4,6,8,8,4,6,3};
+
+        System.out.println("Enter the element you want to add: ");
+        int ele= scanner.nextInt();
+
+        System.out.println("Enter the position you want to add the element: ");
+        int index= scanner.nextInt();
+
+        int[] newArr = new int[arr.length +1 ];
+
+        for (int i=0; i < index; i++){
+            newArr[i]= arr[i];
+
+        }
+
+        newArr[index]= ele;
+
+        for (int j= index; j< arr.length; j++){
+            newArr[j+1]= arr[j];
+        }
+        for (int i=0; i< newArr.length; i++){
+            System.out.print(newArr[i] + "\t");
+        }
+
+
+
+
+
+    }
 }
