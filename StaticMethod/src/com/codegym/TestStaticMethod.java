321463c8b51e6a7a7e81a57721314bf8e2cef51b
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2021-11-21 13:13:40 +0700

    Added folder StaticMethod

---
+++ StaticMethod/src/com/codegym/TestStaticMethod.java
@@ -0,0 +1,17 @@
+package com.codegym;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+        Student.change();
+
+        Student st1= new Student(111, "Hoang");
+        Student st2= new Student(222, "Khanh");
+        Student st3= new Student(333, "Nam");
+
+
+        st1.display();
+        st2.display();
+        st3.display();
+
+    }
+}
