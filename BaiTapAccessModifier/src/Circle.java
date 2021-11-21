1721b1ca53023170b7b9885692a7bb3aeee13301
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2021-11-21 13:37:34 +0700

    Added folder BaiTapAccessModifier

---
+++ BaiTapAccessModifier/src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 1.0;
+        this.color= "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+}
---
+++ BaiTapAccessModifier/src/TestCircle.java
@@ -0,0 +1,10 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle= new Circle(5.0,"blue");
+
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+
+
+}
