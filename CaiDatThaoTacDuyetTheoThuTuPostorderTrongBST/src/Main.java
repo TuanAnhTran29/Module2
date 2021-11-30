5e2a5f0d40ad5c72cdf2ef5c6d9a907885622b9e
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2021-11-30 23:45:40 +0700

    Added folder CaiDatThaoTacDuyetTheoThuTuPostorderTrongBST

---
+++ CaiDatThaoTacDuyetTheoThuTuPostorderTrongBST/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        BST bst= new BST();
+
+        bst.insert(5);
+        bst.insert(4);
+        bst.insert(9);
+        bst.insert(1);
+        bst.insert(7);
+
+        bst.inorder();
+
+        Node root= new Node(5);
+
+        System.out.println(bst.search(root, 1));
+    }
+}
