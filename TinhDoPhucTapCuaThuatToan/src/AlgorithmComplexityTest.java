8127be6e967e3685a7ef6f892bd4097066fced29
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2021-12-01 16:22:51 +0700

    Added folder TinhDoPhucTapCuaThuatToan

---
+++ TinhDoPhucTapCuaThuatToan/src/AlgorithmComplexityTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String input= scanner.nextLine();
+
+        int[] frequentChar= new int[225];
+        for (int i = 0; i < input.length(); i++) {
+            int ascii= (int) input.charAt(i);
+            frequentChar[ascii]+= 1;
+        }
+
+        int max= 0;
+        char character= (char) 225;
+        for (int j=0; j< 225; j++){
+            if (frequentChar[j] > max){
+                max= frequentChar[j];
+                character= (char) j;
+            }
+        }
+        System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+    }
+}
